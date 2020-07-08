3d7bf70cbbad9df814b8bcf2b951133864e95ce7
Daniel Antonio <danieltomantonio@gamil.com>
2020-07-07 19:37:03 -0700

    Finished rest of event listeners.

--- js/index.js
+++ js/index.js
@@ -5,10 +5,37 @@ animate.href = "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate
 document.head.appendChild(animate);
 
 //// DOCUMENT ////
+const container = document.querySelector('.home');
+let busArr = [];
+
 document.body.classList.add('animate__animated');
 document.body.onload = () => document.body.classList.add('animate__fadeIn');
+document.addEventListener('scroll', () => {
+    document.body.style.backgroundColor = "#FFD23F"
+});
 
-document.body.textContent.onselect = () => document.body.style.backgroundColor = "yellow";
+window.onresize = () => {
+    const flex = document.querySelector('.content-section');
+    flex.style.flexDirection = "column";
+}
+
+document.addEventListener('keypress', (e) => {
+    let keyPress = e.key.toLowerCase();
+    if(keyPress === 'b' && busArr.length === 0) {
+        busArr.push('b');
+    } else if (keyPress === 'u' && busArr.length === 1) {
+        busArr.push('u');
+    } else if (keyPress === 's' && busArr.length === 2) {
+        busArr.push('s');
+        alert('USE PROMOCODE: "DESERTBUS" FOR 20% OFF~!!');
+    } else {
+        busArr = [];
+    }
+});
+
+document.addEventListener('touchstart', () => {
+    document.body.style.backgroundColor = "orange";
+})
 
 //// LOGO ////
 const logo = document.querySelector('h1');
@@ -36,11 +63,17 @@ navLinks.forEach(link => link.addEventListener('mouseout', () => {
     link.style.border = '0px'
 }));
 
+navLinks[1].addEventListener('focus', () => document.body.style.backgroundColor = "yellow");
+
 //// IMAGES ////
 const images = document.querySelectorAll('img');
 images[2].addEventListener('dblclick', () => {
     alert("I'M ON A BOAT!");
-})
+});
+
+images[0].addEventListener('drop', () => {
+    alert('WE GOING ON A RIDE HOMIE!');
+});
 
 //// STRETCH ////
 gsap.to(logo, {duration: 1, x: 300, backgroundColor: "#FFD23F"});
